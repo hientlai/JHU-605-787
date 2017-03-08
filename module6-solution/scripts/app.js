e2f5cea4baf50f74269cc293b5b77e6e0c13031e
Hien Lai <hientlai@gmail.com>
2017-03-08 16:29:10 -0500

    add javascript minification

--- module6-solution/scripts/app.js
+++ module6-solution/scripts/app.js
@@ -40,3 +40,10 @@
      return size;
   }
 })();
+
+//Javascript Minification - Remove below commented code for minification
+/*
+
+!function(){"use strict";function e(e,t){e.message="",e.listDishes="",e.messageclass="",e.checkLunch=function(){if(""==e.listDishes)e.message="Please enter data first",e.messageclass="text-danger";else{var t=s(e.listDishes,",");3>=t?(e.message="Enjoy!",e.messageclass="text-success"):(e.message="Too much!",e.messageclass="text-danger")}}}function s(e,s){for(var t=e.split(s),c=0,a=0;a<t.length;a++)""!=t[a].trim()&&(c+=1);return c}angular.module("LunchCheck",[]).controller("LunchCheckController",e),e.$inject=["$scope","$filter"]}();
+
+*/
